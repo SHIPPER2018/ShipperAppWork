08f5d7e6cea26ed7c411705271a84f0c6de76ff4
Maayan Rosenthal <maayan.rose1@gmail.com>
2018-04-26 13:20:08 +0300

    Test
    Test

---
+++ app/src/main/java/com/example/shipperapp/ShipperOrCarrierActivity.java
@@ -0,0 +1,38 @@
+package com.example.shipperapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ShipperOrCarrierActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shipper_or_carrier);
+
+        Button shipperButton = (Button) findViewById(R.id.shipperButton);
+        shipperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intentIsAShipper = new Intent(ShipperOrCarrierActivity.this , ShippCat.class);
+                startActivity(intentIsAShipper);
+            }
+
+        });
+
+        Button carrierButton = (Button) findViewById(R.id.carrierButton);
+        carrierButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intentIsACarrier = new Intent(ShipperOrCarrierActivity.this , CarrLogSignIn.class);
+                startActivity(intentIsACarrier);
+            }
+
+        });
+    }
+}
