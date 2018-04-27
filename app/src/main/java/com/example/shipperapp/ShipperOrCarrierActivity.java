99d4dfa8bc2bab0601fe2b9a0620ab089bc25280
Maayan Rosenthal <maayan.rose1@gmail.com>
2018-04-27 04:45:10 +0300

    Updated - JSON parsing using GSon.. From JAVA to JSON and from JSON to JAVA.

--- app/src/main/java/com/example/shipperapp/ShipperOrCarrierActivity.java
+++ app/src/main/java/com/example/shipperapp/ShipperOrCarrierActivity.java
@@ -1,8 +1,8 @@
 package com.example.shipperapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +13,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 
 public class ShipperOrCarrierActivity extends AppCompatActivity {
@@ -24,14 +23,19 @@ public class ShipperOrCarrierActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        // myRef.setValue("Hello, World - Testing!"); // --> test conn. to DB
+        //DatabaseReference mDatabase = database.getReference().child("Dealers").child();
+        //DatabaseReference myRef = ref.child("test1");
+        DatabaseReference myRef = database.getReference("shipper2018-c6925");
+
 
         //JAVA to JSON - Carrier
         Carrier carrier_test = new Carrier(10000, "he", "hello2","hello3","hello4","hello5");
         Gson gsonObj_java_to_json = new Gson();
         String jsonStrJson = gsonObj_java_to_json.toJson(carrier_test); // converts object to json string
-        myRef.setValue(jsonStrJson); // Push object to Firebase
+//        myRef.setValue(jsonStrJson); // Push object to Firebase
+        myRef.push().setValue(jsonStrJson);
+
+//        myRef.setValue("Hello, World - Testing!"); // --> test conn. to DB
 
         // JSON string to JAVA Object - Carrier
         Gson gson = new Gson();
@@ -39,16 +43,21 @@ public class ShipperOrCarrierActivity extends AppCompatActivity {
         Carrier carrier_json_str_to_java= gson.fromJson(jsonInString, Carrier.class);
         Gson gsonObj_json_to_java = new Gson();
         String jsonStrJava = gsonObj_json_to_java.toJson(carrier_json_str_to_java); // converts object to json string
-        myRef.setValue(jsonStrJava);
+//        myRef.setValue(jsonStrJava);
+
+
+
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
+                for (DataSnapshot messageSnapshot: dataSnapshot.getChildren()) {
+//                    String name = (String) messageSnapshot.child("name").getValue();
+                }
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d(TAG, "Value is: " + value);
             }
 
             @Override
